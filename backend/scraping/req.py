7f46f55f1722df383d4a25cd833fd8f019da2068
James Garfield <jordan@grupojvm.com>
2021-09-04 20:20:59 -0500

    Added image url validation

--- backend/scraping/req.py
+++ backend/scraping/req.py
@@ -1,16 +1,17 @@
 import requests
 
 
-def make_GET_request(url):
+def make_GET_request(url, headers=None):
     """
     Make a GET request to <url> and get back the HTML page content.
 
     Params:
         - <url: str> the url
+        - <headers: dict> custom headers
     
     Return: <html_page: Response>
     """
-    page = requests.get(url)
+    page = requests.get(url, headers=headers)
     return page
 
 
@@ -27,4 +28,26 @@ def valid_url(url):
         make_GET_request(url)
         return True
     except:
+        return False
+
+
+def valid_image_url(url):
+    """
+    Validate that a url contains the correct headers.
+
+    Params:
+        - <url: str> the url to validate.
+        - <headers: tuple(str)> headers
+
+    Return: <bool>
+    """
+    image_formats = ("image/png", "image/jpeg", "image/gif")
+
+    # Grab url headers
+    headers_from_url = requests.get(url)
+    print(headers_from_url.headers)
+    # Check
+    if headers_from_url.headers['content-type'] in image_formats:
+        return True
+    else:
         return False
