8d39b91484b623c52d49958949b10dc2765ea183
James Garfield <jordan@grupojvm.com>
2021-09-04 20:20:15 -0500

    Download model can decide the date for you

--- backend/data/models/download_model.py
+++ backend/data/models/download_model.py
@@ -1,11 +1,13 @@
-from data.helper import DOWNLOADS_TABLE, DOWNLOAD_ID, DOWNLOAD_TITLE, DOWNLOAD_NAME, DOWNLOAD_IMAGE, DOWNLOAD_LOC, DOWNLOAD_DATE
+from backend.data.helper import DOWNLOADS_TABLE, DOWNLOAD_ID, DOWNLOAD_TITLE, DOWNLOAD_NAME, DOWNLOAD_IMAGE, DOWNLOAD_LOC, DOWNLOAD_DATE
+import time
+
 
 class Download:
-    def __init__(self, title, name, date, location, image, id=None):
+    def __init__(self, title, name, location, image, date=None, id=None):
         self.id = id
         self.title = title
         self.name = name
-        self.date = date
+        self.date = date or int(time.time())
         self.location = location
         self.image = image
     
