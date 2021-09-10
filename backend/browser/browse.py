35f8a303bcfb23ace34c90a1eb5a4fc9b124a5c9
James Garfield <jordan@grupojvm.com>
2021-09-09 22:23:01 -0500

    Added some options to bypass cloudflare. Still failing for LightNovelPub currently.

--- backend/browser/browse.py
+++ backend/browser/browse.py
@@ -1,7 +1,9 @@
 # Script for interacting with web through selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import cloudscraper
 
 
 class InteractiveBrowser:
@@ -10,6 +12,7 @@ class InteractiveBrowser:
         self.browser = browser
         if not self.browser:
             self.__setup_browser(initial_url, debug)
+        self.cloud_scrape = cloudscraper.create_scraper()
 
     def current_url(self):
         return self.browser.current_url
@@ -24,9 +27,21 @@ class InteractiveBrowser:
         options = Options()
         if not debug:
             options.headless = True
-            options.add_argument("--log-level=1")
+
+        # Options to bypass cloudflare detection
+        options.add_argument("--log-level=1")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("start-maximized")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
 
         self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        # Bypass cloudflare detection
+        self.browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        self.browser.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+
+        # Clear all cookies and cache
+        self.browser.delete_all_cookies()
         self.browser.get(url)
     
     def get_page(self):
@@ -74,13 +89,13 @@ class InteractiveBrowser:
         (tag_type, tag_value) = tag
         # If else to grab correct data
         if tag_type == "id":
-            tags = self.browser.find_elements_by_id(tag_value)
+            tags = self.browser.find_elements(By.ID, tag_value)
         elif tag_type == "class":
-            tags = self.browser.find_elements_by_class_name(tag_value)
+            tags = self.browser.find_elements(By.CLASS_NAME, tag_value)
         elif tag_type == "name":
-            tags = self.browser.find_elements_by_name(tag_value)
+            tags = self.browser.find_elements(By.TAG_NAME, tag_value)
         elif tag_type == "x-path":
-            tags = self.browser.find_elements_by_xpath(tag_value)
+            tags = self.browser.find_elements(By.XPATH, tag_value)
         else:
             tags = []
 
@@ -101,3 +116,4 @@ class InteractiveBrowser:
         Close the connection on the interactive browser.
         """
         self.browser.close()
+        self.cloud_scrape.close()
