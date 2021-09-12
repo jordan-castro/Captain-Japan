735ae38432f0eb9b9220d97df79220d9b97e46cc
James Garfield <jordan@grupojvm.com>
2021-09-12 16:09:16 -0500

    Full models

--- backend/data/models/download_model.py
+++ backend/data/models/download_model.py
@@ -1,30 +1,29 @@
-from backend.data.helper import DOWNLOADS_TABLE, DOWNLOAD_ID, DOWNLOAD_TITLE, DOWNLOAD_NAME, DOWNLOAD_IMAGE, DOWNLOAD_LOC, DOWNLOAD_DATE, DOWNLOAD_TYPE
+from backend.data.helper import DOWNLOAD_ID, DOWNLOAD_NAME, DOWNLOAD_LOC, DOWNLOAD_DATE, DOWNLOAD_SCRAPE_ID, DOWNLOAD_USER_ID
 import time
 
 
 class Download:
-    def __init__(self, title, name, location, image, download_type, date=None, id=None):
+    def __init__(self, name, location, user_id, scrape_id=None, date=None, id=None):
         self.id = id
-        self.title = title
         self.name = name
         self.date = date or int(time.time())
         self.location = location
-        self.image = image
-        self.type = download_type
-    
-    def sql_format(self):
+        self.user_id = user_id
+        self.scrape_id = scrape_id
+
+
+    def sql_format(self, scrape_id=None):
         """
         Return object in sql object.
 
         Return: <dict>
         """
         data = {
-            DOWNLOAD_TITLE: self.title,
             DOWNLOAD_NAME: self.name,
             DOWNLOAD_DATE: self.date,
             DOWNLOAD_LOC: self.location,
-            DOWNLOAD_IMAGE: self.image or None,
-            DOWNLOAD_TYPE: self.type
+            DOWNLOAD_USER_ID: self.user_id,
+            DOWNLOAD_SCRAPE_ID: self.scrape_id or scrape_id
         }
 
         # Check if id exists and put it on if it does
@@ -32,3 +31,19 @@ class Download:
             data[DOWNLOAD_ID] = self.id
         
         return data
+
+    @staticmethod
+    def from_sql(row):
+        """
+        Create a Download object from a SQL row.
+
+        Return <Download>
+        """
+        return Download(
+            id=row[DOWNLOAD_ID],
+            name=row[DOWNLOAD_NAME],
+            user_id=row[DOWNLOAD_USER_ID],
+            scrape_id=row[DOWNLOAD_SCRAPE_ID],
+            location=row[DOWNLOAD_LOC],
+            date=row[DOWNLOAD_DATE]
+        )
---
+++ backend/data/models/scrape_model.py
@@ -0,0 +1,45 @@
+from backend.data.helper import SCRAPE_TABLE, SCRAPE_TYPE, SCRAPE_SOURCE, SCRAPE_COVER, SCRAPE_TITLE, SCRAPE_ID, DbHelper
+
+
+class Scrape:
+    def __init__(self, type, source, title, id=None, cover=None):
+        self.type = type
+        self.source = source
+        self.title = title
+        self.id = id
+        self.cover = cover
+
+    def sql_format(self):
+        """
+        Convert the Scrape object to it's SQL counterpart.
+
+        Returns: <dict>
+        """
+        row = {
+            SCRAPE_TYPE: self.type,
+            SCRAPE_TITLE: self.title,
+            SCRAPE_SOURCE: self.source
+        } 
+        # Check if ID is passed
+        if self.id:
+            row[SCRAPE_ID] = self.id
+        # Check if cover is passed
+        if self.cover:
+            row[SCRAPE_COVER] = self.cover
+        # Return row
+        return row
+
+    @staticmethod
+    def from_sql(row):
+        """
+        Convert a SQL row to Scrape object.
+
+        Returns: <Scrape>
+        """
+        return Scrape(
+            type=row[SCRAPE_TYPE],
+            source=row[SCRAPE_SOURCE],
+            title=row[SCRAPE_TITLE],
+            id=row[SCRAPE_ID],
+            cover=row[SCRAPE_COVER]
+        )
