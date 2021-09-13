94b23671c36a3157f54c3ded3b72cd6013df1bc1
James Garfield <jordan@grupojvm.com>
2021-09-13 11:27:10 -0500

    Cleaned up downloader for universal support.

--- backend/utils/downloader.py
+++ backend/utils/downloader.py
@@ -1,4 +1,5 @@
-### Script to handle the downloads and generations.
+# Script to handle the downloads and generations.
+from backend.scraping.scraper_base import Scraper
 from backend.data.models.download_model import Download
 from multiprocessing.pool import ThreadPool
 from backend.generator.html_gen import generate_html_file
@@ -6,119 +7,113 @@ from backend.generator.generate_manga import generate_manga
 from backend.utils.default_title import default_title
 
 
-def download_novel(novel_title, chapters, scrape_method, async_=True):
+class Downloader:
     """
-    Download a novel.
-
-    Params:
-        - <novel_title: str> The title of the novel to download.
-        - <chapters: list(int)> The chapters to download.
-        - <scrape_method: def> The method that will scrape the data
-        - <async_: bool=True> Should we run this method asynchronously?
-
-    Return: <list(Download)>
+    Handle all downloads from scraping and such.
     """
-    downloads = []
-    # Title of novel universal
-    title = default_title(novel_title)
-    if async_:
-        # Pool for quicker speed
+
+    def download(self, title: str, chapters: list, scraper: Scraper, t: int, async_=True):
+        """
+        Download the scraped source from the internet.
+
+        Params:
+            - <title: str> The title of the novel/manga/anime.
+            - <chapters: list(int)> a list of integers.
+            - <scraper: Scraper> a scraper object.
+            - <t: int> The type or scraping being done. Novel,Manga,Anime
+            - <async_: bool=True> Whether or not we should run this asynchronoulsy. Default is true.
+
+        Returns: <list(Download)>
+        """
+        self.scraper = scraper
+        self.t = t
+
+        downloads = []
+        title = default_title(title)
+
+        if async_:
+            data = self.__download_async(chapters)
+        else:
+            data = self.__download_sync(chapters)
+
+        # Now do the download
+        for chp in data:
+            # File name
+            if "chapter_title" in chp:
+                file_name = chp["chapter_title"]
+            else:
+                file_name = f"Chapter_{data[data.index(chp)]}"
+
+            if self.t == 0:
+                # Add a download
+                download = generate_html_file(chp, file_name, title)
+                downloads.append(
+                    Download(
+                        name=file_name,
+                        location=download,
+                        user_id=1
+                    )
+                )
+            elif self.t == 1:
+                # Add the downloads
+                download = generate_manga(chp, title)
+                map(
+                    lambda path: downloads.append(
+                        Download(
+                            name=file_name,
+                            location=path,
+                            user_id=1
+                        )
+                    ),
+                    download
+                )
+
+        return downloads
+
+    def __download_async(self, chapters):
         pool = ThreadPool(processes=len(chapters))
-
-        # Requests
+        # Hold the requests
         reqs = []
+        # Hold the chapter data
+        _chapters = []
+        # Loop through chapters
+        for chapter in chapters:
+            # Apply asynchronus
+            if self.t == 0:
+                a = pool.apply_async(self.scraper.scrape, (chapter, ))
+            elif self.t == 1:
+                self.scraper.change_page(self.scraper.build_url(chapter))
+                a = pool.apply_async(
+                    self.scraper.scrape,
+                    (
+                        chapter, self.scraper.get_page()
+                    )
+                )
 
+            # Add to requests
+            reqs.append(a)
 
-        # Start the process for reading chapter data
-        for chapter in chapters:    
-            reqs.append(pool.apply_async(scrape_method, (chapter, )))
-
-        # Now grab the data
         for req in reqs:
-            # Data
-            chapter_data = req.get()
-            # Verify data
-            if not chapter_data:
+            # Grab data
+            data = req.get()
+            # Check compiler suggestions
+            if not data:
                 continue
 
-            # Name of file
-            file_name = f"Chapter_{chapters[reqs.index(req)]}"
-            # Write file
-            file_path = generate_html_file(chapter_data, file_name, directory=title)
-            downloads.append(
-                Download(
-                    name=file_name,
-                    location=file_path,
-                    user_id=1
-                )
-            )
-    else:
-        # For synchronous calls. Required when webpage needs to run javascript
+            _chapters.append(data)
+
+        return _chapters
+
+    def __download_sync(self, chapters):
+        # Hold the chapter_data
+        _chapters = []
         for chapter in chapters:
-            # Grab data
-            chapter_data = scrape_method(chapter)
-            file_name = f"Chapter_{chapter}"
-            file_path = generate_html_file(chapter_data, file_name, directory=title)
-            # Add to downloads
-            downloads.append(
-                Download(
-                    name=file_name,
-                    location=file_path,
-                    user_id=1
-                )
-            )
+            if self.t == 0:
+                data = self.scraper.scrape(chapter)
+            elif self.t == 1:
+                self.scraper.change_page(self.scraper.build_url(chapter))
+                data = self.scraper.scrape(chapter, self.scraper.get_page())
 
-    # Now return download data
-    return downloads
+            _chapters.append(data)
 
-
-def download_manga(manga_title, chapters, scraper):
-    """
-    Download Manga.
-
-    Params:
-        - <manga_title: str> The title of the manga.
-        - <chapters: list(int)> The chapters to download.
-        - <scraper: Object> The scraper object.
-
-    Return: <list(Download)>
-    """
-    # Thread to save time
-    pool = ThreadPool(processes=len(chapters))
-
-    reqs = []
-    title = default_title(manga_title)
-
-    # Request data
-    for chapter in chapters:
-        # Change to correct chapter
-        scraper.browser.change_page(scraper.build_url(chapter))
-        # Apply the request
-        reqs.append(pool.apply_async(scraper.scrape, (chapter, scraper.browser.get_page())))
-
-    for req in reqs:
-        # Data from req
-        chapter_data = req.get()
-        # Verify
-        if not chapter_data:
-            continue
-
-        # Generate manga files
-        paths = generate_manga(chapter_data, title)
-
-    # Verify content
-    if not paths:
-        return
-    
-    return list(
-        map(
-            lambda path: Download(
-                title=title,
-                name=path.split('/')[-1].replace('.png', ''),
-                location=path,
-                image=None,
-                download_type=2
-            ),
-            paths
-        )
-    )
+        return _chapters
