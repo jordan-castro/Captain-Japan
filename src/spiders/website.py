9273529b98f3739ab7e27e52e6dc0feba16f0f68
James Garfield <jordan@grupojvm.com>
2021-11-11 13:08:27 -0500

    A description tag.

--- src/spiders/website.py
+++ src/spiders/website.py
@@ -13,10 +13,11 @@ class Website:
     """
     Data manager for a website.
     """
-    def __init__(self, base_url, title_tag: Tag, body_tag: Tag, cover_image_tag: Tag, chapters_tag: Tag, scrape_type: ScrapeType) -> None:
+    def __init__(self, base_url, title_tag: Tag, body_tag: Tag, cover_image_tag: Tag, chapters_tag: Tag, description_tag: Tag, scrape_type: ScrapeType) -> None:
         self.base_url = base_url
         self.title_tag = title_tag
         self.body_tag = body_tag
         self.cover_image_tag = cover_image_tag
         self.chapters_tag = chapters_tag
+        self.description_tag = description_tag
         self.scrape_type = scrape_type
