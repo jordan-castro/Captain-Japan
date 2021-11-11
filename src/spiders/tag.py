05e0e0f6f16478ba229cf909e68aa3298b6a01a6
James Garfield <jordan@grupojvm.com>
2021-11-11 13:08:18 -0500

    Added beatiful soup capabs

--- src/spiders/tag.py
+++ src/spiders/tag.py
@@ -5,13 +5,33 @@ class Tag(object):
     """
     Oject to hold the tag data for a scraper.
 
-    Properties:
+    Attributes:
         tag_type: By
             The tage type, whether its a class, id, name, or x-path
         tag_value: str
             The value of the tag.
     """
-    def __init__(self, tag_type: By, tag_value: str) -> None:
+    def __init__(self, tag_type: By, tag_value: str, tag: str = None) -> None:
         self.tag_type = tag_type
-        self.tag_value = tag_value        
+        self.tag_value = tag_value
+        self.tag = tag
         super().__init__()
+
+    @property
+    def tag_type_soup(self):
+        """
+        Returns the tag type as a string.
+
+        Returns:
+            str: The tag type as a string.
+        """
+        if self.tag_type == By.CLASS_NAME:
+            return 'class'
+        elif self.tag_type == By.ID:
+            return 'id'
+        elif self.tag_type == By.NAME:
+            return 'name'
+        elif self.tag_type == By.XPATH:
+            return 'xpath'
+        else:
+            return None
