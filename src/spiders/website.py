426d4e3d3670a5a836de7255e7f2a9ecd251772c
James Garfield <jordan@grupojvm.com>
2021-11-16 22:20:26 -0500

    spiders/website: Reformat and chapters_args

--- src/spiders/website.py
+++ src/spiders/website.py
@@ -1,6 +1,6 @@
 import enum
 
-from src.spiders.tag import Tag
+from src.spiders.tag import Tag, Args
 
 
 class ScrapeType(enum.Enum):
@@ -13,7 +13,17 @@ class Website:
     """
     Data manager for a website.
     """
-    def __init__(self, base_url, title_tag: Tag, body_tag: Tag, cover_image_tag: Tag, chapters_tag: Tag, description_tag: Tag, scrape_type: ScrapeType) -> None:
+    def __init__(
+        self, 
+        base_url, 
+        title_tag: Tag, 
+        body_tag: Tag, 
+        cover_image_tag: Tag, 
+        chapters_tag: Tag, 
+        description_tag: Tag, 
+        scrape_type: ScrapeType,
+        chapters_args: Args=None
+    ) -> None:
         self.base_url = base_url
         self.title_tag = title_tag
         self.body_tag = body_tag
@@ -21,3 +31,4 @@ class Website:
         self.chapters_tag = chapters_tag
         self.description_tag = description_tag
         self.scrape_type = scrape_type
+        self.chapters_args = chapters_args
