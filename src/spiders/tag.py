61df49fbc0661b8c3a7d1157ec5bae114314493a
James Garfield <jordan@grupojvm.com>
2021-11-16 22:20:17 -0500

    spiders/tag: Add Args object

--- src/spiders/tag.py
+++ src/spiders/tag.py
@@ -10,6 +10,8 @@ class Tag(object):
             The tage type, whether its a class, id, name, or x-path
         tag_value: str
             The value of the tag.
+        tag: str
+            The tag itself.
     """
     def __init__(self, tag_type: By, tag_value: str, tag: str = None) -> None:
         self.tag_type = tag_type
@@ -35,3 +37,38 @@ class Tag(object):
             return 'xpath'
         else:
             return None
+
+
+class Args(object):
+    """
+    The arguments for a Website.
+
+    Attributes:
+        types: list[str]
+            The type of the arguments to pass to the url.
+        values: list[str]
+            The values of the arguments to pass to the url.
+    """
+    def __init__(self, types: list[str], values: list[str]=[]) -> None:
+        self.types = types
+        self.values = values
+        super().__init__()
+
+    def value_for(self, type_: str, value=None) -> str:
+        """
+        Return the value for the type.
+
+        Params:
+            - type_: str
+                The type of the value.
+            - value: str
+                The value to return if the type is not found.
+
+        Returns:
+            str: The value for the type.
+        """
+        # Return the value.
+        if self.types.index(type_) < len(self.values):
+            return self.values[self.types.index(type_)]
+        else:
+            return value
