bde7afcfb9722992e73bc4169d3ee93d110f7fe5
James Garfield <jordan@grupojvm.com>
2021-12-23 18:00:09 -0500

    Added App settings

---
+++ cj/utils/settings.py
@@ -0,0 +1,45 @@
+import json
+from typing import Any
+
+
+SETTINGS_FILE = "data/app.json"
+
+
+def read_settings(key: str) -> Any:
+    """
+    Read the settings.
+
+    Params:
+        - key(str): The key to read the JSON from
+
+    Returns:
+        - value(Any)
+    """
+    # Read the settings file
+    with open(SETTINGS_FILE, 'r') as file:
+        settings = json.load(file)
+
+        # Check if the key even exists
+        if key not in settings:
+            return None
+
+        return settings[key]
+
+
+def set_settings(key: str, value):
+    """
+    Set a setting.
+
+    Params: 
+        - key(str): The key to set.
+        - value(Any): The value to set.
+    """
+    # Read the settings file
+    with open(SETTINGS_FILE, 'r') as file:
+        settings = json.load(file)
+
+    settings[key] = value
+
+    # Write the settings file
+    with open(SETTINGS_FILE, 'w') as file:
+        json.dump(settings, file)
