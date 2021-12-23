ddcbba6b7ca3606fd604704d371f060da35cc087
James Garfield <jordan@grupojvm.com>
2021-12-23 18:00:46 -0500

    Added new path methods

--- cj/utils/path.py
+++ cj/utils/path.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from cj.objects.novel import Novel
+from cj.utils.settings import read_settings
 
 
 def create_dir(path):
@@ -20,3 +22,31 @@ def create_dir(path):
 
     # Create the directory
     path.mkdir()
+
+
+def create_novel_path(novel: Novel):
+    """
+    Create the directories of a novel.
+    
+    Params:
+        - novel(Novel): The Novel to create the directories for.
+    """
+    # Get the novel path
+    novel_path = read_settings("path")['source'] + f'/novels/{novel.title}'
+    # Create the novel directory
+    create_dir(novel_path)
+    return novel_path
+
+
+def create_chapter_path(chapter: int, parent: str):
+    """
+    Create the chapters of a parent (Novel, Manga, Anime)
+
+    Params:
+        - chapter(int): The chapter to create the directories for, this should be the index of the chapter.
+        - parent(str): The parent directory to create the chapter directories in.
+    """
+    # Get the chapter path
+    chapter_path = parent + f'/{chapter}'
+    # Create the chapter directory
+    create_dir(chapter_path)
