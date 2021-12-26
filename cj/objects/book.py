be871a82a7e0aa79303d3a0bc2ccf6248347d46c
James Garfield <jordan@grupojvm.com>
2021-12-26 16:04:42 -0500

    Book object

---
+++ cj/objects/book.py
@@ -0,0 +1,71 @@
+from cj.utils.enums import BookType
+from cj.utils.naming import remove_chars
+
+
+class Book:
+    title: str = None
+    author: str = None
+    location: str = None
+    
+    def __init__(self, title=None, author=None, location=None) -> None:
+        self.title = title
+        self.author = author
+        self.location = location
+
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author}"
+
+    def __repr__(self) -> str:
+        return f"{self.title} by {self.author}"
+    
+    def __eq__(self, other) -> bool:
+        return self.title == other.title and self.author == other.author and self.location == other.location
+
+    @property
+    def book_type(self) -> BookType:
+        """
+        Get the book type from a location string.
+        """
+        # Raise exception if location is None
+        if self.location is None:
+            raise ValueError("No Book location provided.")
+        
+        # Strip by the last . to get the extension
+        extension = self.location.split(".")[-1]
+        return BookType.from_str(extension)
+
+    def _chapter_numbers(self):
+        # Requires that the location is set
+        if self.location is None:
+            raise ValueError("No Book location provided.")
+        
+        # Get the chapter number from the location
+        if '\\' in self.location:
+            splitter = '\\'
+        else:
+            splitter = '/'
+        
+        # Get the last part of the location
+        last_part = self.location.split(splitter)[-1]
+        chapters = last_part.split('-')[-1]
+        # Remove the ext
+        chapters = chapters.split('.')[0]
+        # Split again by _
+        chapters = chapters.split('_')
+        # Return the first part
+        return [int(chapter.split()) for chapter in chapters]
+
+
+    @property
+    def starting_chapter(self) -> int:
+        """
+        Return the starting chapter of the book.
+        """
+        return min(self._chapter_numbers())
+
+    @property
+    def ending_chapter(self) -> int:
+        """
+        Return the ending chapter of the book.
+        """
+        return max(self._chapter_numbers())
--- cj/utils/enums.py
+++ cj/utils/enums.py
@@ -15,3 +15,35 @@ class CjType(Enum):
             return "anime"
         else:
             raise ValueError("Invalid CjType")
+
+
+class BookType(Enum):
+    EPUB = 0
+    PDF = 1
+    MOBI = 2
+    HTML = 3
+
+    def __str__(self):
+        if self == BookType.EPUB:
+            return "epub"
+        elif self == BookType.PDF:
+            return "pdf"
+        elif self == BookType.MOBI:
+            return "mobi"
+        elif self == BookType.HTML:
+            return "html"
+        else:
+            raise ValueError("Invalid BookType")
+    
+    @staticmethod
+    def from_str(string: str):
+        if string == "epub":
+            return BookType.EPUB
+        elif string == "pdf":
+            return BookType.PDF
+        elif string == "mobi":
+            return BookType.MOBI
+        elif string == "html":
+            return BookType.HTML
+        else:
+            raise ValueError("Invalid BookType")
