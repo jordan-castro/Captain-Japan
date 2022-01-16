42c44907f82158d2c161fa665e75ce0911a75073
James Garfield <jordan@grupojvm.com>
2022-01-16 15:46:47 -0500

    get file name from path function

--- cj/utils/naming.py
+++ cj/utils/naming.py
@@ -1,6 +1,8 @@
 # Methods for naming in CaptainJapan
 import re
 
+from cj.utils.path import get_slash_type
+
 
 def remove_chars(string: str) -> str:
     """
@@ -11,3 +13,18 @@ def remove_chars(string: str) -> str:
     string = re.sub(r'[^\w\s]', '', string)
     string = re.sub(r'[\s]+', ' ', string)
     return string.strip()
+
+
+def get_file_name(file_path: str, extension: bool = False) -> str:
+    """
+    Get the name of a file based on it's path.
+    Also works to get a file with an extension if extension is passed as True.
+    """
+    slash = get_slash_type(file_path)
+
+    file_name = file_path.split(slash)[-1]
+
+    if extension:
+        return file_name
+    else:
+        return file_name.split('.')[0]
