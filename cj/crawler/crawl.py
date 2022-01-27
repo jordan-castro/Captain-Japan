63ff69c5a901e122d143f195445338271e80e4d6
James Garfield <jordan@grupojvm.com>
2022-01-27 10:56:02 -0500

    Implemented crawler with timer to not DDOs

--- cj/crawler/crawl.py
+++ cj/crawler/crawl.py
@@ -1,7 +1,56 @@
+from cj.objects.chapter import Chapter
+from cj.objects.novel import Novel
+from cj.scraper.novel_base import NovelBase
+from cj.utils.enums import CjType
+from threading import Thread
+
+import time
+
+from cj.utils.save import Save
+
+
 class Crawler:
     """
     This class handles the calling of the scraper.
     """
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, source, cj_type: CjType, time_between_scrape: int = 10) -> None:
+        self.source = source
+        self.type = cj_type
+        self.time_between_scrape = time_between_scrape
+
+    def crawl(self, chapters: list[int]) -> None:
+        """
+        Crawl the chapters passed.
+        """
+        if self.type == CjType.MANGA:
+            pass
+        elif self.type == CjType.NOVEL:
+            # Set the Base class
+            self.source: NovelBase = self.source
+            novel: Novel = Novel(None, self.source.title, None, None)
+            # Grab chapters
+            chapters_url = self.source.search()
+            if chapters_url is None:
+                raise Exception("No chapters found.")
+            # Load the chapters
+            self.source.load_chapters(chapters_url)
+            for i in chapters:
+                # Grab the chapter based on index
+                chapter = self.source.chapters[i]
+                if chapter.number is None:
+                    chapter.number = i
+                chapter = self.source.scrape(chapter)
+                # Save the chapter assynchronously
+                save = Save(chapter, novel)
+                Thread(target=save.save).start()
+                # Wait the time_between_scrape
+                time.sleep(self.time_between_scrape)
+            # Quit the scraper and join the current thread
+            self.source.quit()
+
+            # Some CMD L
+            print(f"Finished scraping {self.source.title}, You can find the source in {novel.location}.")
+
+        else:
+            raise NotImplementedError("Anime is not yet supported.")
